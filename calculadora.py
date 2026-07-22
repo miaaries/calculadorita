2c55dc9e19da15f5435285f2dff0decdbe75a41c
miaaries <magodelascavernas7@gmail.com>
2026-07-22 15:43:47 -0500

    Update calculadora.py
    agregar la resta Mia Calderon

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,22 @@
-## CALCULADORA - VERSIÓN 1
-# Autor(a): NOMBRE APELLIDO
+## CALCULADORA - VERSIÓN 2
+# Autor(a): Mia Calderon 
 # ===========================================
+print("calculadora")
+
+
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
-suma = num1 + num2
+print("\n 1.sumar")
+print(" n 2.restar")
 
-print("Resultado:", suma)
+opcion= input("\n seleccione una opcion ")
+
+if opcion =="1":
+  print("\n la suma es:",num1 +num2)
+
+elif opcion == "2":
+  print("\n la resta es:",num1 -num2)
+
+else:
+  print("\n opcion no valida )
